0cc3feb82f6cd49ec70744d38d14b93713e78246
Mohamed <bola13368@gmail.com>
2022-11-07 04:26:17 +0200

    navbar design

---
+++ src/Components/LeftBar/LeftBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LeftBar.scss";
+
+function LeftBar() {
+  return <div className="leftBar">LeftBar</div>;
+}
+
+export default LeftBar;
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Register.scss";
 
 function Register() {
@@ -13,7 +14,9 @@ function Register() {
             consequatur.
           </p>
           <span>Do you have an account?</span>
-          <button>Login</button>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
         </div>
         <div className="right">
           <h1>Register</h1>
